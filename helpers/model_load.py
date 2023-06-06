212ca911ee498129d30305da89976861ce9885b6
t3du <32546729+t3du@users.noreply.github.com>
2023-06-06 19:42:53 -0400

    Update model_load.py

--- helpers/model_load.py
+++ helpers/model_load.py
@@ -88,6 +88,11 @@ def load_model(root, load_on_run_all=True, check_sha256=True, map_location="cuda
         path_extend = ""
 
     model_map = {
+        "maturemalemix_v14.safetensors": {
+            'sha256': '0f5b5d7b9cd029dc12fde669209fdb2446e318c17f019b8e381a37e9743ec7aa',
+            'url': "https://civitai.com/api/download/models/75441",
+            'requires_login': False,
+        },
          "mistoonAmethyst_v20.safetensors": {
             'sha256': 'fc896c44f11404efbb59b73bbf08a9265afb7a905104ca9626b92e5998f078d2',
             'url': "https://civitai.com/api/download/models/56710",
